c09ed311644a9537eb80a72dc9bacc836cc3553c
ludo <mariani.ludovico.studente@itispaleocapa.it>
2024-01-11 10:51:43 +0100

    Modifica  a Interfaccia

---
+++ Mariani_Test/Domanda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mariani_Test
+{
+    internal class Domanda : IComponent
+    {
+        public Domanda() { }
+
+        public void Aggiunta(IComponent component)
+        {
+
+        }
+
+        public void Rimuovi(int index)
+        {
+
+        }
+    }
+}
---
+++ Mariani_Test/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mariani_Test
+{
+    internal class Test : IComponent
+    {
+
+
+    }
+}
