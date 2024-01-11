acaaa3516c8e17303ada04160dfc64556ade60cd
ludo <mariani.ludovico.studente@itispaleocapa.it>
2024-01-11 10:32:45 +0100

    Aggiungere i file di progetto.

---
+++ Mariani_Test/Composite.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mariani_Test
+{
+    public abstract class Component
+    {
+        public abstract void Aggiunta(Component componente);
+
+        public abstract void Rimuovi(int index);
+
+        public new abstract bool Equals(object obj);
+
+        public new abstract string ToString();
+    }
+}
