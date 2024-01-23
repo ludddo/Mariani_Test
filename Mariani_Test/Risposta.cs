3d5e71d3d87105e4e7deb386c494d116430f9ddf
Ludovico Mariani <mariani.ludovico.studente@itispaleocapa.it>
2024-01-23 13:08:00 +0100

    Scheletro Classi Finito

--- Mariani_Test/Risposta.cs
+++ Mariani_Test/Risposta.cs
@@ -8,5 +8,72 @@ namespace Mariani_Test
 {
     internal class Risposta
     {
+        private string _testo;
+        private int _punti;
+
+        public string Testo
+        {
+            get { return _testo; }
+            set { _testo = value; }
+        }
+
+        public int Punti
+        {
+            get { return _punti; }
+            set { _punti = value; }
+        }
+
+        public Risposta()
+        {
+            Testo = "";
+            Punti = 0;
+        }
+
+        public Risposta(string testo, int punti)
+        {
+            Testo = testo;
+            Punti = punti;
+        }
+
+        public Risposta(Risposta oldRisposta)
+        {
+            Testo = oldRisposta.Testo;
+            Punti = oldRisposta.Punti;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Risposta) || obj == null)
+                return false;
+
+            Risposta other = (Risposta)obj;
+
+            return Testo == other.Testo && Punti == other.Punti;
+        }
+
+        public void Aggiunta(IComponent comp)
+        {
+
+        }
+
+        public void Rimuovi(int index)
+        {
+
+        }
+
+        public IComponent GetChild(int index)
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"{Testo};{Punti}";
+        }
+
+        public double Punteggio()
+        {
+            return Punti;
+        }
     }
 }
